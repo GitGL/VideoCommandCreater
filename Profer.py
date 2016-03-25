127104dcef7cb404d38be49367ddc255cc88dc48
Guolei <sdweihaiguolei@163.com>
2016-03-25 21:03:15 +0800

    change with VideoPic

--- Profer.py
+++ Profer.py
@@ -10,7 +10,8 @@ def main():
     mp.InitCmdFile()
 
     # mp.CaptureImage("00:04:15.000","00:04:25.000","/media/hustrc/LinuxData/Download/Video/Mobile Suit Gundam 00 Second Season Batch/E01[The Angels' Second Advent].mkv",30)
-    mp.CaptureImage("/media/guolei/L-Data/TV/cmd")
+    # mp.CaptureImage("/media/guolei/L-Data/TV/cmd")
+    mp.CaptureImage("/media/guolei/L-Data/TV")
             
 if __name__ == '__main__':
     main()
